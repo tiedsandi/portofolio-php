5948c48fc5a26ce7abe822ee5d26af5b99ecdf19
tiedsandi <fachransandi@gmail.com>
2025-03-21 12:59:27 +0700

    first commit

---
+++ admin/inc/sidebar.php
@@ -0,0 +1,32 @@
+<div class="sidebar">
+    <div class="sidebar-content">
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+            <a href="#" class="navbar-brand">Navigation</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=home">Home<i class="fa fa-home"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=about">About<i class="fa fa-address-card"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=experience">Experience<i class="fa fa-star"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=service">Service<i class="fa fa-tasks"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=portfolio">Portfolio<i class="fa fa-file-archive"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=contact">Contact<i class="fa fa-envelope"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
---
+++ admin/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Resume Website Template Free Download</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Resume Website Template Free Download" name="keywords">
+    <meta content="Resume Website Template Free Download" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300;400;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- CSS Libraries -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="../lib/slick/slick.css" rel="stylesheet">
+    <link href="../lib/slick/slick-theme.css" rel="stylesheet">
+    <link href="../lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="51">
+    <div class="wrapper">
+        <?php include "inc/sidebar.php"; ?>
+        <div class="content">
+        <?php 
+            if(isset($_GET['page']) && file_exists('content/'. $_GET['page'] .'.php')) {
+                include 'content/'. $_GET['page'] .'.php';
+            }else {
+                include 'content/about-me.php';
+            }
+        ?>
+        </div>
+    </div>
+
+    <!-- Back to Top -->
+    <a href="#" class="back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="../lib/easing/easing.min.js"></script>
+    <script src="../lib/slick/slick.min.js"></script>
+    <script src="../lib/typed/typed.min.js"></script>
+    <script src="../lib/waypoints/waypoints.min.js"></script>
+    <script src="../lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="../lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="../js/main.js"></script>
+</body>
+
+</html>
